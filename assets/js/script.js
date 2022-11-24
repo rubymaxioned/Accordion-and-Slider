77e7ae4abce8cdbf2e976896ce5a63dbb53c26a8
rubymaxioned <axioned0482022>
2022-11-24 14:35:11 +0530

    span

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var accordion = document.querySelectorAll('.accordion-head span')
-//console.log(accordion);
+var horizontalIcon = document.querySelectorAll('.accordion-head span::after');
+console.log(horizontalIcon);
 
 accordion.forEach(function(list){
     console.log(list);
