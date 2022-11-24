009f36aea49b31123b06b7bf5e42618358b31a52
rubymaxioned <axioned0482022>
2022-11-24 15:54:12 +0530

    image specificity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 var accordion = document.querySelectorAll('.accordion-head span')
 var para = document.querySelectorAll('.accordion li p')
-// console.log(para[2]);
+var images = document.querySelectorAll('.slider-images li');
+var arrow = document.querySelectorAll('.arrow li');
+
+// console.log(arrow);
 
 accordion.forEach(function(list,index){
     list.addEventListener('click',function(){
         list.classList.toggle('active');
         para[index].classList.toggle('show');
         console.log(para[index]);
+    })
+})
+
+arrow.forEach(function(btn){
+    btn.addEventListener('click',function(){
+        images[1].classList.add('show');
     })
 })
